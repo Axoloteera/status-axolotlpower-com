4c303cdd61ccf083f2cf53b7b6729f449b7c4d18
Gtd232 <im.gtd232@outlook.com>
2024-01-27 21:23:55 +0800

    move to src and make files prettier

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@
 import template from './template.html';
 
 export default {
-  async fetch(request, env) {
-    let value = {}
-    const keys = await env.STATUS_INFO.list();
-    //console.log(keys);
-    for (const key of keys["keys"]) {
-      value[key.name] = await env.STATUS_INFO.get(key.name);
-    }
-    //console.log(value);
+	async fetch(env) {
+		let value = {};
+		const keys = await env.STATUS_INFO.list();
+		//console.log(keys);
+		for (const key of keys['keys']) {
+			value[key.name] = await env.STATUS_INFO.get(key.name);
+		}
+		//console.log(value);
 
-    return new Response(template.replace("$DATA", JSON.stringify(value)), {
-        headers: { 'Content-Type': 'text/html' }
-    });
-  }
+		return new Response(template.replace('$DATA', JSON.stringify(value)), {
+			headers: { 'Content-Type': 'text/html' },
+		});
+	},
 };
-
