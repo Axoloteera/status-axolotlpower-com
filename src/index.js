03f56f5ee2700957981026d87afcd5f172a1c6a7
Gtd232 <im.gtd232@outlook.com>
2024-01-28 11:14:23 +0800

    根据数据自动显示服务是否在线

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ export default {
 		const keys = await env.STATUS_INFO.list();
 		//console.log(keys);
 		for (const key of keys['keys']) {
+      if(key.name == 'NOTICE') continue;
 			value[key.name] = await env.STATUS_INFO.get(key.name);
 		}
 		//console.log(value);
 
-		return new Response(template.replace('$DATA', JSON.stringify(value)), {
+		return new Response(template.replace('$DATA', JSON.stringify(value)).replace("$NOTICE", keys['keys'].filter(key => key.name == 'NOTICE')), {
 			headers: { 'Content-Type': 'text/html' },
 		});
 	},
